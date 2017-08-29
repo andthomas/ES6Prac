f9076ff24623db72a00c5bc5d680f9e4bdf185ec
Andrew Brown <andrewtcb@gmail.com>
2017-08-29 17:12:57 +1000

    Started string and int helper methods

--- app/index.js
+++ app/index.js
@@ -22,12 +22,64 @@
 
 
 
-const limit = 200;
+// let limit = 200;
+//
+// {
+//   //we only want a limit of 10 people back stage
+//   let limit = 10;
+//   console.log('backstage limit', limit)
+// }
+//
+// console.log('overall venue limit', limit);
 
-{
-  //we only want a limit of 10 people back stage
-  const limit = 10;
-  console.log('backstage limit', limit)
-}
 
-console.log('overall venue limit', limit);
+
+// let a = `good`;
+// let greeting = `${a} morning`;
+// console.log(greeting);
+
+
+
+// let a = [20, 30, 40];
+// let b = [10, ...a, 50];
+//
+// console.log(b)
+
+
+// function collect(...a){
+//   console.log(a);
+// }
+//
+// collect(3, 6, 9, 12, 15);
+
+
+// let animals = ['Simba', 'Zazu', 'Ed'];
+// let [lion, bird] = animals;
+//
+// console.log(lion, bird);
+
+
+// let king = { name: 'Mufasa', kids: 1 };
+// let { name, kids } = king;
+// console.log(name, kids);
+
+// setTimeout(() => {
+//   console.log('Woohoo!');
+// }, 3000);
+
+
+// let cheer = () => {
+//   console.log("Whoohoo!");
+// }
+
+// let values = [20, 30, 40];
+//
+// let doubled = values.map((n) => {
+//   return n*2 });
+// console.log(doubled);
+
+let points = [10, 2, 23, 43, 3, 4, 1]
+
+let highScores = points.filter((n) => {return n>10})
+
+console.log(highScores);
